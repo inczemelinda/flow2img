8aa9121bf6cfeaaf4b52d5f9b0dc71ee35703070
Incze Melinda Henrietta <icz.melinda@gmail.com>
2026-01-14 14:28:47 +0200

    Initial commit: src, results, documentation

---
+++ src/notebooks/config.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+PROJECT_ROOT = Path(r"E:\Project\flow2img")
+
+RAW_CSV_DIR = PROJECT_ROOT / "data" / "raw" / "csv"
+RAW_PCAP_DIR = PROJECT_ROOT / "data" / "raw" / "pcap"
+
+IMAGES_DIR = PROJECT_ROOT / "data" / "images"
+RUNS_DIR = PROJECT_ROOT / "runs"
